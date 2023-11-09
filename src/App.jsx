e5cc3132292075f71f0cc9d66c97c228dcf9a136
kevindimayuga <k.dimayuga22@gmail.com>
2023-11-08 22:04:09 -0600

    installed tailwindcss and created component files

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,17 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+import AboutMe from './components/AboutMe'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import NavBar from './components/NavBar'
+import Portfolio from './components/Portfolio'
+import Resume from './components/Resume'
+
+
 function App() {
   const [count, setCount] = useState(0)
 
